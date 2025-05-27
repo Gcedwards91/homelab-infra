5f7b7dab9064e4e50cff95f9ad7a6ef4bbcc0cd8
Cliff Edwards <gcedwards91@gmail.com>
2025-05-27 17:54:36 -0400

    changing providers

--- terraform/firstDeployment/provider.tf
+++ terraform/firstDeployment/provider.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
-      version = ">= 2.9.6"
+      source  = "local/proxmox"
+      version = ">= 2.9.14"
     }
   }
 }
