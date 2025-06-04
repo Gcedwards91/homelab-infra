f41ff80a17167ad8b5da906d0f5c7d34aed5d8ae
Cliff Edwards <gcedwards91@gmail.com>
2025-06-04 14:04:23 -0400

    adjusting main.tf bios settings for almalinux

--- terraform/firstDeployment/main.tf
+++ terraform/firstDeployment/main.tf
@@ -73,8 +73,8 @@ resource "proxmox_vm_qemu" "alma_vm" {
  #  boot = "order=scsi0"
 
   bios = "ovmf"
-  machine = "q35"
-  
+  #machine = "q35"
+
   os_type = "cloud-init"
 
   ipconfig0 = "ip=dhcp"
