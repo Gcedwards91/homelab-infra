43fe7dde251fb0ff4441ee909ad4dea14223c8b2
Cliff Edwards <gcedwards91@gmail.com>
2026-05-02 04:15:21 +0000

    chore: remove dead code - python/, container-project/, fix .dockerignore typo

--- weather-app/python/statporter/testnpipe.py
+++
@@ -1,8 +0,0 @@
-import docker
-
-try:
-    client = docker.DockerClient(base_url="npipe:////./pipe/docker_engine")
-    version = client.version()
-    print("Docker version info:", version)
-except Exception as e:
-    print("Error:", e)
